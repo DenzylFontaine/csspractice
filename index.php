b8d09cdb59cab18dc4ba820c8e932141ba09da97
DenzylFontaine <ddenzfong@gmail.com>
2016-01-14 23:45:30 -0700

    images added

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	</head>
 	<body>
 		<h1>Music of this Generation</h1>
-		<img src="https://slm-assets2.secondlife.com/assets/4730505/lightbox/MaleDancerSilhouette"
+		<img src="https://slm-assets2.secondlife.com/assets/4730505/lightbox/MaleDancerSilhouette.jpg?1323414439"
 			<img src="http://i3.kym-cdn.com/photos/images/facebook/000/263/418/5e7" />
 		<div class="bdr-crm id=paragraph-1">
 		<p>There's always some great music out there, just gotta know where to look.</p>
