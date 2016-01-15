fa81d977c7b8319e5d5919687ca115b8bd7b4131
DenzylFontaine <ddenzfong@gmail.com>
2016-01-14 23:33:42 -0700

    .gitignore was added

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE htmhl>
+<html>
+	<head>
+		<title>Practice Makes perfect</title>
+	</head>
+	<body>
+		<h1>Music of this Generation</h1>
+		<img src="http://cache4.asset-cache.net/xd/146372132"
+			<img src="http://i3.kym-cdn.com/photos/images/facebook/000/263/418/5e7" />
+		<div class="bdr-crm id=paragraph-1">
+		<p></p>
+		</div>
+	</body>
+</html>
