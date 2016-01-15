33a41b69e86c815a50c99b4aafeaa03a83c180d9
DenzylFontaine <ddenzfong@gmail.com>
2016-01-15 00:01:39 -0700

    stylesheet added

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE htmhl>
-<html>
+<html lang="en">
 	<head>
+		<meta charset="utf-8">
+		<link href="stylesheet.css" rel="stylesheet" type="text/css" />
 		<title>Practice Makes perfect</title>
 	</head>
 	<body>
 		<h1>Music of this Generation</h1>
-		<img src="https://slm-assets2.secondlife.com/assets/4730505/lightbox/MaleDancerSilhouette.jpg?1323414439"
-			<img src="http://i3.kym-cdn.com/photos/images/facebook/000/263/418/5e7" />
+		<img class="pic" src="https://slm-assets2.secondlife.com/assets/4730505/lightbox/MaleDancerSilhouette.jpg?1323414439"
+			<img class="pic" src="http://i3.kym-cdn.com/photos/images/facebook/000/263/418/5e7.jpg" />
 		<div class="bdr-crm id=paragraph-1">
 		<p>There's always some great music out there, just gotta know where to look.</p>
 		</div>
