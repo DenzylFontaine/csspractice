73f87b55ae515b21a2f165e4b007b9618da846c3
DenzylFontaine <ddenzfong@gmail.com>
2016-01-15 00:03:27 -0700

    stylesheet added

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<body>
 		<h1>Music of this Generation</h1>
 		<img class="pic" src="https://slm-assets2.secondlife.com/assets/4730505/lightbox/MaleDancerSilhouette.jpg?1323414439"
-			<img class="pic" src="http://i3.kym-cdn.com/photos/images/facebook/000/263/418/5e7.jpg" />
+			<img class="pic" src="http://i3.kym-cdn.com/photos/images/facebook/000/263/418/5e7" />
 		<div class="bdr-crm id=paragraph-1">
 		<p>There's always some great music out there, just gotta know where to look.</p>
 		</div>
