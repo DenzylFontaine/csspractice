7031f757eda235a40d8f8e03e7020d7c70f14916
DenzylFontaine <ddenzfong@gmail.com>
2016-01-15 19:55:52 -0700

    css changes

--- index.php
+++ index.php
@@ -6,12 +6,26 @@
 		<title>Practice Makes perfect</title>
 	</head>
 	<body>
-		<h1>Music of this Generation</h1>
-		<img class="pic bg-purp" src="https://slm-assets2.secondlife.com/assets/4730505/lightbox/MaleDancerSilhouette.jpg?1323414439"
+		<header class="head">
+			<h1>Distracions</h1>
+			<p>who's beefing?<br/>
+			New break-ups!
+			Photos Photos Photos!
+			</p>
+		</header>
+		<h2>Music of this Generation</h2>
+		<img class="pic" src="https://slm-assets2.secondlife.com/assets/4730505/lightbox/MaleDancerSilhouette.jpg?1323414439"
 		<br/>
 		<img class="pic" src="http://img.memecdn.com/walk-into-the-club_o_1497591.jpg" />
 		<div class="bdr-crm id=paragraph-1">
-		<p>There's always some great music out there, just gotta know where to look.</p>
+		<p>There's always some great music out there, just gotta know where to look.<br>
+			Sorry rock fans but yes im a hater, just sounds like screaming to me.<br/>
+			Country after living in West Virgina its hard not to like country.<br/>
+			The creativity is rediculous y'all literally sing about anything.<br/>
+			Hip hop is hot right now and yeah there are some diamonds amoust the whole lot of bs.<br/>
+			Rap is king (totally unbiased)! Kendrick is the king of rap! logical next step...<br/>
+			Kendrick for president 2024! You heard it here first! ;)
+		</p>
 		</div>
 	</body>
 </html>
