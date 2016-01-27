887ac2df9440e5f06deb82d8d53b7ee6eca4fa91
DenzylFontaine <ddenzfong@gmail.com>
2016-01-27 13:00:20 -0700

    changed pic

--- index.php
+++ index.php
@@ -1,46 +1,58 @@
-<!DOCTYPE htmhl>
+<!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8">
-		<link href="stylesheet.css" rel="stylesheet" type="text/css" />
-		<title>Practice Makes perfect</title>
+		<meta charset="utf-8"/>
+		​
+		<!-- IE Rendering Mode = Edge-->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		​
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"/>
+		​
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous" />
+		​
+		<!-- LOAD OUR CUSTOM STYLESHEET HERE!!11! -->
+		<link rel="stylesheet" href="style.css" />
+		​
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		​
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		​
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		​
+		<title>Sample Bootstrap Page</title>
 	</head>
 	<body>
-		<header class="head">
-			<h1 class="h1">Distractions</h1>
-			<p>who's beefing?<br/>
-			New break-ups!<br/>
-			Photos Photos Photos!<br/>
-			</p>
-		</header>
-		<h2>Music of this Generation</h2>
-		<img class="pic" src="https://upload.wikimedia.org/wikipedia/commons/d/de/Sunset_Party_Dancing_Girl_Silhouette.jpg"
-		<br/>
-		<img class="pic2" src="http://zone1.aqusagtechnologi.netdna-cdn.com/wp-content/uploads/2015/09/dance1.jpg" />
-		<div class="bdr-crm id=paragraph-1">
-		<p>There's always some great music out there, just gotta know where to look.<br>
-			Sorry rock fans but yes im a hater, just sounds like screaming to me.<br/>
-			Country after living in West Virgina its hard not to like country.<br/>
-			The creativity is rediculous y'all literally sing about anything.<br/>
-			Hip hop is hot right now and yeah there are some diamonds amoust the whole lot of bs.<br/>
-			Rap is king (totally unbiased)! Kendrick is the king of rap! logical next step...<br/>
-			Kendrick for president 2024! You heard it here first! ;)
-		</p>
+		<div class="container">
+			<div class="row" >
+				<div class="row">
+					<h1>RPG</h1>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<p>
+					column 1
+				<p/>
+			</div>
+			<div class="col-md-4">
+				<p>
+					column 2
+				</p>
+			</div>
+			<div class="col-md-4">
+				<p>
+					column 3
+				</p>
+			</div>
 		</div>
-		<h3>Genres</h3>
-		<p>
-			<ul>
-			<li>Classic Rock</li>
-			<li>Rock and Roll</li>
-			<li>Heavy Metal</li>
-			<li>Rap</li>
-			<li>R&B</li>
-			<li>Country</li>
-			<li>Techno</li>
-			<li>Trance</li>
-			<li>Classical</li>
-			<li>Folk Song</li>
-		</ul>
-		</p>
 	</body>
 </html>
